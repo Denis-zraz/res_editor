a8d83c03f67a04a74bd0985312f0c20edacaa26b
Denis-zraz <den.zrazhevskii@mail.ru>
2025-06-28 11:27:13 +0300

    построение структуры

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
+import Main from './components/Main'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <section className='w-[1120px] h-screen flex flex-col m-auto drop-shadow-2xl bg-white'>
+      <header className='w-full h-16 border-b shadow-inner p-3'>
+        <h1 className="font-['Days_One'] text-4xl text-center">Редактор резюме</h1>
+      </header>
+      <Main/>
+    </section>
   )
 }
 
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+export default function Main() {
+  return (
+    <main className='w-full h-full flex flex-row'>
+        
+    </main>
+  )
+}
